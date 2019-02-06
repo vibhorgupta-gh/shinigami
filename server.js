e61507c8f6fd3d0be7409f2da1450f8c30730b9e
VibhorCodecianGupta <guptavibhu98@gmail.com>
2019-02-06 09:45:41 +0530

    stateless service with es6 support

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const mongoose = require('mongoose')
-const { url } = require('./config.js')
+import express from 'express'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended : true }))
-
-mongoose.connect(url, { useNewUrlParser : true })
-mongoose.connection.on('error', ()=>{ console.log('Error connecting to database') })
-mongoose.connection.once('open', ()=>{ console.log('Connection made') })
 
 app.listen(4000, ()=>{
   console.log('Server running')
