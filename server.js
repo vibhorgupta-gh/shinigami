dbd2de6503072394d2efe33877eba2f77f7c9ec3
VibhorCodecianGupta <guptavibhu98@gmail.com>
2019-02-11 21:21:57 +0530

    logging service

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const router = require('./routes/route.js')
+const morgan = require('morgan')
+const winston = require('./middleware/logger.js')
 
 const app = express()
 
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(morgan('combined', { stream: winston.stream }))
 
 app.use('/', router)
 app.get('*', (req, res, next) => { res.status(404).send('404: Not found') })
